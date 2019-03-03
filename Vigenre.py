b1e90c733cd90e8bcd40b5862974cfdfaa60cb7b
Coleman Nugent <colemannugent@gmail.com>
2019-03-02 22:09:10 -0800

    Vigenre.py: Add Vigenre implementation

--- Vigenre.py
+++ Vigenre.py
@@ -1,9 +1,46 @@
 from CipherInterface import *
+import math
 
 class Vigenre(CipherInterface):
 
+	def table_lookup(self, plain, key, reverse=False):
+		pindex = ord(plain) - 65
+		kindex = ord(key) - 65
+
+		if not reverse:
+			offset = (pindex + kindex) % 26
+		else:
+			offset = (pindex - kindex) % 26
+
+		return chr(offset + 65)
+
+
 	def encrypt(self, plaintext):
-		return "Encrypted text!\n"
+		plaintext = plaintext.upper()
+
+		ciphertext = ""
+
+		# repeat the key enough to cover the plaintext
+		length = math.ceil(len(plaintext) / len(self.key))
+		keytext = self.key * length
+		print(keytext)
+
+		for p,k in zip(plaintext, keytext):
+			ciphertext += self.table_lookup(p, k)
+
+		return ciphertext
 
 	def decrypt(self, ciphertext):
-		return "Decrypted text!"
+		ciphertext = ciphertext.upper()
+
+		plaintext = ""
+
+		# repeat the key enough to cover the plaintext
+		length = math.ceil(len(ciphertext) / len(self.key))
+		keytext = self.key * length
+		print(keytext)
+
+		for p,k in zip(ciphertext, keytext):
+			plaintext += self.table_lookup(p, k, reverse=True)
+
+		return plaintext
