a544511b4948689cb6f58ed61a334c8dad9c9f95
Coleman Nugent <colemannugent@gmail.com>
2019-03-05 15:53:45 -0800

    cipher.py: Change arguments to match assignment

--- cipher.py
+++ cipher.py
@@ -24,23 +24,21 @@ valid_ciphers = ["PLF", "RTS", "RFC", "VIG", "CES", "MAC"]
 parser = argparse.ArgumentParser(description='Encrypt and decrypt using six different ciphers.', formatter_class=argparse.RawTextHelpFormatter)
 
 # Add our arguments to the parser
-parser.add_argument("cipher", help="""Name of the cipher to use
-PLF - Playfair
-RTS - Row Transposition
-RFC - Railfence
-VIG - Vigenere
-CES - Caesar
-MAC - Monoalphabetic""")
+parser.add_argument("cipher", help="""Name of the cipher to use. One of:
+	PLF - Playfair
+	RTS - Row Transposition
+	RFC - Railfence
+	VIG - Vigenere
+	CES - Caesar
+	MAC - Monoalphabetic""")
 
 parser.add_argument("key", help="The key to use for encryption/decryption")
-parser.add_argument("-e", "--encrypt", help="Run the cipher in encryption mode", action='store_true')
-parser.add_argument("-d", "--decrypt", help="Run the cipher in decryption mode", action='store_true')
+parser.add_argument("mode", help="Run the cipher in ENC (encryption) or DEC (decryption) mode")
 parser.add_argument("input", help="The path to the input file")
 parser.add_argument("output", help="The path to the output file")
-# TODO: add list of ciphers to help
 
 # Make sure we have enough arguments, if not print the help message
-if len(sys.argv) < 4:
+if len(sys.argv) < 5:
 	parser.print_help()
 	sys.exit(1)
 
@@ -51,7 +49,6 @@ arguments = parser.parse_args()
 cipher = CipherInterface()
 
 assert arguments.cipher in valid_ciphers, "Cipher not recognized. Use --help for more info."
-assert (arguments.encrypt or arguments.decrypt), "Must specify either --encrypt or --decrypt. Use --help for more info."
 
 # set cipher to specified cipher
 if(arguments.cipher == "PLF"):
@@ -75,7 +72,7 @@ inputFile = open(arguments.input, "r")
 outputFile = open(arguments.output, "w")
 
 # Perform the encryption/decryption
-if(arguments.encrypt):
+if(arguments.mode == "ENC"):
 	outputFile.write(cipher.encrypt(inputFile.read()))
 else:
 	outputFile.write(cipher.decrypt(inputFile.read()))
