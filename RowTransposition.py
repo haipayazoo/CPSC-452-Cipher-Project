6b5e1c36d8fd81499e8844eebf054048a40674a8
Cameron Mathos <haipayazoo@gmail.com>
2019-03-05 14:13:28 -0800

    Update RowTransposition.py

--- RowTransposition.py
+++ RowTransposition.py
@@ -1,9 +1,35 @@
 from CipherInterface import *
+import math
 
 class RowTransposition(CipherInterface):
+    
+    def encrypt(self, plaintext):
+        cols=len(self.key)
+        rows=math.ceil(len(plaintext)/cols)
+        padd=['x','y','z','a','b','c','d']
+        x=0
+        key=list(self.key)
+        cyphertext=''
+        while len(plaintext)<rows*cols:
+            plaintext=plaintext+padd[x]
+            x=(x+1)%len(padd)
+        current=0
+        matrix=[['' for j in range(cols)]for i in range(rows)]
+        plainindex=0
+        for r, row in enumerate(matrix):
+            for c, _ in enumerate(row):
+                matrix[r][c]=plaintext[plainindex]
+                plainindex=plainindex+1
+        for num in key:
+            for row in matrix:
+                cyphertext=cyphertext+row[int(num)-1]
+        return cyphertext
 
-	def encrypt(self, plaintext):
-		return "Encrypted text!\n"
+    def decrypt(self, ciphertext):
+        return "Decrypted text!"
 
-	def decrypt(self, ciphertext):
-		return "Decrypted text!"
+
+#test code
+#x=RowTransposition()
+#x.setKey('3421567')
+#print(x.encrypt('transpositionciphers'))
