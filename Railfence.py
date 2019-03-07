37683266f8bd60cb4668d285acce6d8beeb43af0
Cameron Mathos <haipayazoo@gmail.com>
2019-03-06 16:50:17 -0800

    Railfence encryption gives correct output.

--- Railfence.py
+++ Railfence.py
@@ -3,29 +3,32 @@ from CipherInterface import *
 class Railfence(CipherInterface):
 
 	def encrypt(self, plaintext):
-		rail = [['\n' for i in range(len(plaintext))] for j in range(int(self.key))]
-		dir_down = False
+		rail = [['\n' for j in range(len(plaintext))] for i in range(int(self.key))]
+		wrap = True
 		row, col = 0, 0
+
 		for i in range(len(plaintext)):
 			if(row == 0) or (row == int(self.key) - 1):
-				dir_down = not dir_down
+				wrap = not wrap
 
 			rail[row][col] = plaintext[i]
 			col += 1
+			row += 1
 
-			if dir_down:
-				row += 1
-			else:
-				row -= 1
+			if wrap:
+				row = 0
+
 		result =[]
+
 		for i in range(int(self.key)):
 			for j in range(len(plaintext)):
 				if rail[i][j] != '\n':
 					result.append(rail[i][j])
+
 		return("" . join(result))
 
 	def decrypt(self, ciphertext):
-			rail = [['\n' for i in range(len(ciphertext))] for j in range(int(self.key))]
+			rail = [['\n' for j in range(len(ciphertext))] for i in range(int(self.key))]
 
 			dir_down = None
 			row, col = 0, 0
