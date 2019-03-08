3d7e848fb85179dcdf6359ec754004e2158dce3f
Christopher Bos <cbos95@csu.fullerton.edu>
2019-03-08 00:05:59 -0800

    cleaned up this script and added both small.txt and big.txt tests

--- test_ciphers.sh
+++ test_ciphers.sh
@@ -4,52 +4,36 @@ GREEN='\033[0;32m'
 RED='\033[0;31m'
 NC='\033[0m'
 
-echo "Testing Playfair Cipher (small.txt)"
-CIPHER=PLF
-KEY=security
-FILE_IN=small.txt
-FILE_ENC=plf_small_enc.txt
-FILE_DEC=plf_small_dec.txt
-./cipher.py $CIPHER $KEY ENC $FILE_IN $FILE_ENC
-./cipher.py $CIPHER $KEY DEC $FILE_ENC $FILE_DEC
-cmp --silent $FILE_IN $FILE_DEC && { echo -e "$CIPHER ($FILE_IN): ${GREEN}SUCCESS${NC}"; rm $FILE_ENC; rm $FILE_DEC; } || { echo -e "$CIPHER ($FILE_IN): ${RED}FAILURE${NC}: check $FILE_IN, $FILE_ENC and $FILE_DEC for errors"; }
+CIPHERS=( PLF RTS RFC VIG CES )
+KEYS=( security 3421567 3 deceptive 5 )
 
-echo "Testing Row Transposition Cipher (small.txt)"
-CIPHER=RTS
-KEY=3421567
+echo "##### small.txt #####"
 FILE_IN=small.txt
-FILE_ENC=rts_small_enc.txt
-FILE_DEC=rts_small_dec.txt
-./cipher.py $CIPHER $KEY ENC $FILE_IN $FILE_ENC
-./cipher.py $CIPHER $KEY DEC $FILE_ENC $FILE_DEC
-cmp --silent $FILE_IN $FILE_DEC && { echo -e "$CIPHER ($FILE_IN): ${GREEN}SUCCESS${NC}"; rm $FILE_ENC; rm $FILE_DEC; } || { echo -e "$CIPHER ($FILE_IN): ${RED}FAILURE${NC}: check $FILE_IN, $FILE_ENC and $FILE_DEC for errors"; }
+for c in {0..4}
+do
+	CIPHER=${CIPHERS[c]}
+	KEY=${KEYS[c]}
+	FILE_ENC=${CIPHER}_small_enc.txt
+	FILE_DEC=${CIPHER}_small_dec.txt
+	echo "Testing $CIPHER Cipher (${FILE_IN})"
+	./cipher.py $CIPHER $KEY ENC $FILE_IN $FILE_ENC
+	./cipher.py $CIPHER $KEY DEC $FILE_ENC $FILE_DEC
+	sed -i -e '$a\' $FILE_DEC # add a newline onto the end of the decrypted file
+	cmp --silent $FILE_IN $FILE_DEC && { echo -e "$CIPHER ($FILE_IN): ${GREEN}SUCCESS${NC}"; rm $FILE_ENC; rm $FILE_DEC; } || { echo -e "$CIPHER ($FILE_IN): ${RED}FAILURE${NC}: check $FILE_IN, $FILE_ENC and $FILE_DEC for errors"; }
+done
 
-echo "Testing Railfence Cipher (small.txt)"
-CIPHER=RFC
-KEY=3
-FILE_IN=small.txt
-FILE_ENC=rfc_small_enc.txt
-FILE_DEC=rfc_small_dec.txt
-./cipher.py $CIPHER $KEY ENC $FILE_IN $FILE_ENC
-./cipher.py $CIPHER $KEY DEC $FILE_ENC $FILE_DEC
-cmp --silent $FILE_IN $FILE_DEC && { echo -e "$CIPHER ($FILE_IN): ${GREEN}SUCCESS${NC}"; rm $FILE_ENC; rm $FILE_DEC; } || { echo -e "$CIPHER ($FILE_IN): ${RED}FAILURE${NC}: check $FILE_IN, $FILE_ENC and $FILE_DEC for errors"; }
-
-echo "Testing Vigenere Cipher (small.txt)"
-CIPHER=VIG
-KEY=deceptive
-FILE_IN=small.txt
-FILE_ENC=vig_small_enc.txt
-FILE_DEC=vig_small_dec.txt
-./cipher.py $CIPHER $KEY ENC $FILE_IN $FILE_ENC
-./cipher.py $CIPHER $KEY DEC $FILE_ENC $FILE_DEC
-cmp --silent $FILE_IN $FILE_DEC && { echo -e "$CIPHER ($FILE_IN): ${GREEN}SUCCESS${NC}"; rm $FILE_ENC; rm $FILE_DEC; } || { echo -e "$CIPHER ($FILE_IN): ${RED}FAILURE${NC}: check $FILE_IN, $FILE_ENC and $FILE_DEC for errors"; }
-
-echo "Testing Caesar Cipher (small.txt)"
-CIPHER=CES
-KEY=5
-FILE_IN=small.txt
-FILE_ENC=ces_small_enc.txt
-FILE_DEC=ces_small_dec.txt
-./cipher.py $CIPHER $KEY ENC $FILE_IN $FILE_ENC
-./cipher.py $CIPHER $KEY DEC $FILE_ENC $FILE_DEC
-cmp --silent $FILE_IN $FILE_DEC && { echo -e "$CIPHER ($FILE_IN): ${GREEN}SUCCESS${NC}"; rm $FILE_ENC; rm $FILE_DEC; } || { echo -e "$CIPHER ($FILE_IN): ${RED}FAILURE${NC}: check $FILE_IN, $FILE_ENC and $FILE_DEC for errors"; }
+echo ""
+echo ""
+echo "##### big.txt #####"
+FILE_IN=big.txt
+for c in {0..4}
+do
+	CIPHER=${CIPHERS[c]}
+	KEY=${KEYS[c]}
+	FILE_ENC=${CIPHER}_big_enc.txt
+	FILE_DEC=${CIPHER}_big_dec.txt
+	echo "Testing $CIPHER Cipher (${FILE_IN})"
+	./cipher.py $CIPHER $KEY ENC $FILE_IN $FILE_ENC
+	./cipher.py $CIPHER $KEY DEC $FILE_ENC $FILE_DEC
+	cmp --silent $FILE_IN $FILE_DEC && { echo -e "$CIPHER ($FILE_IN): ${GREEN}SUCCESS${NC}"; rm $FILE_ENC; rm $FILE_DEC; } || { echo -e "$CIPHER ($FILE_IN): ${RED}FAILURE${NC}: check $FILE_IN, $FILE_ENC and $FILE_DEC for errors"; }
+done
